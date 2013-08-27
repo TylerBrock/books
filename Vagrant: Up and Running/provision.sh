0ba7cd87ed50668683c9eb943fc8ae8211965721
Tyler Brock <tyler.brock@gmail.com>
2013-08-27 19:11:36 -0400

    Vagrant up and running

---
+++ Vagrant: Up and Running/provision.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+
+echo "Installing MongoDB..."
+apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv 7F0CEB10
+echo 'deb http://downloads-distro.mongodb.org/repo/ubuntu-upstart dist 10gen' | sudo tee /etc/apt/sources.list.d/10gen.list
+
+apt-get update
+apt-get install mongodb-10gen
+
